40ba0e36d7a40a873ce22a3c39b3ab6d447273bb
PROFESSOR473247 <nikiton123456789@yandex.ru>
2025-11-01 09:29:06 +0300

    Update BOT_TOKEN and reformat config.py

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Токен из переменных окружения (безопасно)
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8373568269:AAHrydlLKaV9JdkyEavzMDLYHHTW_h9gW1o")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8373568269:AAGSBjS57tRHXumLRPWBAe2lUeFq7NNzhl0")
 
 if not BOT_TOKEN:
+
     raise ValueError("❌ BOT_TOKEN не найден! Убедитесь, что переменная окружения установлена.")
