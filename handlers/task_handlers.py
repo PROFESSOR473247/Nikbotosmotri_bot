ed78efb13b7b7c47f6d5c752acc7413b7d442f22
PROFESSOR473247 <nikiton123456789@yandex.ru>
2025-11-13 20:20:06 +0300

    Update task_handlers.py

--- handlers/task_handlers.py
+++ handlers/task_handlers.py
@@ -1,4 +1,4 @@
-fromtelegram import Update, ReplyKeyboardMarkup
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ContextTypes, ConversationHandler, CommandHandler, MessageHandler, filters
 from keyboards.task_keyboards import (
     get_tasks_main_keyboard, get_groups_keyboard,
